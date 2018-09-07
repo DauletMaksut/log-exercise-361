04e766da4771e98d983b3f2adcd376470f93d837
Daulet <daulet.maksut01@gmail.com>
2018-09-07 16:18:33 +0600

    task

--- src/main/java/kz/edu/nu/cs/exercise/MyServlet.java
+++ src/main/java/kz/edu/nu/cs/exercise/MyServlet.java
@@ -6,10 +6,15 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 
 @WebServlet(urlPatterns = { "/myservlet" })
 public class MyServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
+
+//Save all request before
+    private ArrayList<String> story = new ArrayList<String>();
 
     public MyServlet() {
         super();
@@ -17,7 +22,14 @@ public class MyServlet extends HttpServlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        response.getWriter().append("Served at: ").append(request.getContextPath());
+        LocalDateTime time = LocalDateTime.now();
+        String host = request.getRemoteHost();
+        String path = request.getContextPath();
+        String res = "Path: " + path +" Time: " + time.toString() +  " Host: " + host;
+        story.add(res);
+        for(String element : story) {
+            response.getWriter().append(element).append('\n');
+        }
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
